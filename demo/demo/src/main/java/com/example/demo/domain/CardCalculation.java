45af0b93e7a11010b5274e822e1e61e10f31eda0
nayuta-ichino <nayuta.ichino@icloud.com>
2026-07-02 10:14:04 +0900

    CardCalculation(Dmain)に[@Component]を付与

--- demo/demo/src/main/java/com/example/demo/domain/CardCalculation.java
+++ demo/demo/src/main/java/com/example/demo/domain/CardCalculation.java
@@ -1,9 +1,11 @@
 package com.example.demo.domain;
 
+import org.springframework.stereotype.Component;
+
 import com.example.demo.entity.CardEntity;
 
+@Component
 public class CardCalculation{
-
     public int calculateFinalScore(CardEntity card, int limitBreakCount){
         return card.getBaseScore() + (limitBreakCount * card.getScorePerLimitBreak());
     }
