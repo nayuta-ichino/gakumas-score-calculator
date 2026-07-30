eee742a51d43b1f3581a299df368a79252b1f627
nayuta-ichino <nayuta.ichino@icloud.com>
2026-07-30 15:03:33 +0900

    chore:demoフォルダ内のコードを変更してテスト2

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -13,3 +13,5 @@
 --     values('TEST2', 'SR', 'Dance', 2, 8);
 -- INSERT INTO card(name, rarity, type, base_score, score_per_limit_break)
 --     values('TEST3', 'SSR', 'Visial', 5, 2);
+-- INSERT INTO card(name, rarity, type, base_score, score_per_limit_break)
+--     values('TEST4', 'SR', 'Visial', 6, 3);
